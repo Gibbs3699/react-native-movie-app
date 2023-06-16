36b78acbe1d97db7566f529b6583e0c30a6553c0
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-06-16 14:43:33 +0700

    add header to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,25 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { styles } from '../theme'
+import {Bars3CenterLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView>
-            <View>
-                <Text className="text-red-500">HomeScreen</Text>
-            </View>
-        </SafeAreaView>
+        <View className="flex-1 bg-neutral-800">
+            <SafeAreaView>
+                <StatusBar style="light"/>
+                <View className="flex-row justify-between items-center mx-4">
+                    <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white"/>
+                    <Text className="text-white text-3xl font-bold">
+                            <Text style={styles.text}>M</Text>ovies
+                    </Text>
+                    <TouchableOpacity>
+                        <MagnifyingGlassIcon size="30" strokeWidth={2} color="white"/>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        </View>
     )
 }
 
