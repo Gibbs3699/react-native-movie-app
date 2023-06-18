c236a6561bc5863bc430ca592a5a6aa7233984e6
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-06-19 01:01:34 +0700

    fix headder padding

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,14 +1,16 @@
-import { View, Text, ScrollView, SafeAreaView, TouchableOpacity, Platform } from 'react-native'
+import { View, Text, ScrollView, SafeAreaView, TouchableOpacity, Platform, Dimensions } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { styles } from '../theme'
-import { ChevronLeftIcon, HeartIcon } from 'react-native-heroicons/outline'
-import { useRoute } from '@react-navigation/native'
+import { styles, theme } from '../theme'
+import { ChevronLeftIcon, HeartIcon } from 'react-native-heroicons/solid'
+import { useNavigation, useRoute } from '@react-navigation/native'
 
+var { width, height } = Dimensions.get('window')
 const ios = Platform.OS == "ios"
 const topMargin = ios ? "" : "mt-3"
 
 const MovieScreen = () => {
     const {params: item} = useRoute()
+    const navigation = useNavigation()
     useEffect(() => {
 
     }, [item])
@@ -16,12 +18,12 @@ const MovieScreen = () => {
 
     return (
         <ScrollView
-            contentContainerStyle={{ paddingBottom: 20 }}
+            contentContainerStyle={{padding: 20 }}
             className="flex-1 bg-neutral-900"
         >
             <View className="w-full">
                 <SafeAreaView className={"absolute z-20 w-full flex-row justify-between items-center px-4 " + topMargin}>
-                    <TouchableOpacity style={styles.background} className="rounded-xl p-1">
+                    <TouchableOpacity onPress={()=> navigation.goBack()} style={styles.background} className="rounded-xl p-1">
                         <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
                     </TouchableOpacity>
 
