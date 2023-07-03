1f1caa238127aa0dbbbc24142e6a6cfc8730dce4
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-07-03 17:39:14 +0700

    complete SearchScreen UI

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,12 +1,74 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Dimensions, Image } from 'react-native'
+import React, { useState } from 'react'
+import { XMarkIcon } from 'react-native-heroicons/outline'
+import { useNavigation } from '@react-navigation/native'
 
+var { width, height } = Dimensions.get('window')
+let movieName = "Ant-Man and the Wasp: Quantumania"
 const SearchScreen = () => {
-  return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
-  )
+    const navigation = useNavigation()
+    const [results, setResults] = useState([1, 2, 3, 4])
+    return (
+        <SafeAreaView className="flex-1 bg-neutral-800">
+            <View className="mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full">
+                <TextInput
+                    placeholder="Search Movie"
+                    placeholderTextColor={'lightgray'}
+                    className="pb-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider"
+                />
+                <TouchableOpacity
+                    onPress={() => { navigation.navigate('Home') }}
+                    className="rounded-full p-3 m-1 bg-neutral-500"
+                >
+                    <XMarkIcon size="25" color="white" />
+
+                </TouchableOpacity>
+            </View>
+
+            {
+                results.length > 0 ? (
+                    <ScrollView
+                        showsVerticalScrollIndicator={false}
+                        contentContainerStyle={{ paddingHorizontal: 15 }}
+                        className="space-y-3"
+                    >
+                        <Text className="text-white font-semibold ml-1">Result ({results.length})</Text>
+                        <View className="flex-row justify-between flex-wrap">
+                            {
+                                results.map((item, index) => {
+                                    return (
+                                        <TouchableWithoutFeedback
+                                            key={index}
+                                            onPress={() => navigation.push('Movie', item)}
+                                        >
+                                            <View className="space-y-2 mb-4">
+                                                <Image className="rounded-3xl"
+                                                    source={require('../assets/images/moviePoster2.png')}
+                                                    style={{ width: width * 0.44, height: height * 0.3 }}
+                                                />
+                                                <Text className="text-neutral-300 ml-1">
+                                                    {
+                                                        movieName.length > 22 ? movieName.slice(0, 22) + '...' : movieName
+                                                    }
+                                                </Text>
+                                            </View>
+                                        </TouchableWithoutFeedback>
+                                    )
+                                })
+                            }
+                        </View>
+                    </ScrollView>
+                ) : (
+                    <View className="flex-row justify-center">
+                        <Image
+                            source={require('../assets/images/movieTime.png')}
+                            className="h-96 w-96"
+                        />
+                    </View>
+                )
+            }
+        </SafeAreaView>
+    )
 }
 
 export default SearchScreen
