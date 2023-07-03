ca164e0cd0b79567b42029863024b99fd9ee6ade
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-07-03 14:21:46 +0700

    adding details to MovieScreen

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -10,6 +10,7 @@ const ios = Platform.OS == "ios"
 const topMargin = ios ? "" : "mt-3"
 
 const MovieScreen = () => {
+    let movieName = "Ant-Man and the Wasp: Quantumania"
     const { params: item } = useRoute()
     const navigation = useNavigation()
     useEffect(() => {
@@ -20,11 +21,11 @@ const MovieScreen = () => {
     return (
         <ScrollView
             contentContainerStyle={{ paddingBottom: 20 }}
-            className="flex-1 bg-neutral-900"
-        >
+            className="flex-1 bg-neutral-900">
+
             <View className="w-full">
-                <SafeAreaView className={"absolute z-20 w-full flex-row justify-between items-center px-4" + topMargin}>
-                    <TouchableOpacity onPress={() => navigation.goBack()} style={styles.background} className="rounded-xl p-1">
+                <SafeAreaView className={"absolute z-20 w-full flex-row justify-between items-center px-4 " + topMargin}>
+                    <TouchableOpacity style={styles.background} className="rounded-xl p-1" onPress={() => navigation.goBack()}>
                         <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
                     </TouchableOpacity>
 
@@ -34,18 +35,60 @@ const MovieScreen = () => {
                 </SafeAreaView>
                 <View>
                     <Image
+                        // source={require('../assets/images/moviePoster2.png')} 
                         source={require('../assets/images/moviePoster2.png')}
                         style={{ width, height: height * 0.55 }}
                     />
                     <LinearGradient
-                        colors={['transparent', 'rgba(23,23,23,0.8)', 'rgba(23,23,23,1)']}
-                        style={{width, height: height*0.40}}
-                        start={{x:0.5, y:0}}
-                        end={{x:0.5, y:1}}
+                        colors={['transparent', 'rgba(23, 23, 23, 0.8)', 'rgba(23, 23, 23, 1)']}
+                        style={{ width, height: height * 0.40 }}
+                        start={{ x: 0.5, y: 0 }}
+                        end={{ x: 0.5, y: 1 }}
                         className="absolute bottom-0"
                     />
                 </View>
+
+
+
             </View>
+
+            {/* movie details */}
+
+            <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">
+                {/* title */}
+                <Text className="text-white text-center text-3xl font-bold tracking-widest">
+                    {
+                        movieName
+                    }
+                </Text>
+
+                {/* status, release year, runtime */}
+                <Text className="text-neutral-400 font-semibold text-base text-center">
+                    Released • 2020 • 170 min
+                </Text>
+
+
+
+                {/* genres  */}
+                <View className="flex-row justify-center mx-4 space-x-2">
+                    <Text className="text-neutral-400 font-semibold text-base text-center">
+                        Action •
+                    </Text>
+                    <Text className="text-neutral-400 font-semibold text-base text-center">
+                        Thrill •
+                    </Text>
+                    <Text className="text-neutral-400 font-semibold text-base text-center">
+                        Comedy •
+                    </Text>
+                </View>
+
+                {/* description */}
+                <Text className="text-neutral-400 mx-4 tracking-wide">
+                    When a headstrong street orphan, Seiya, in search of his abducted sister unwittingly taps into hidden powers, he discovers he might be the only person alive who can protect a reincarnated goddess, sent to watch over humanity. Can he let his past go and embrace his destiny to become a Knight of the Zodiac?
+                </Text>
+
+            </View>
+
         </ScrollView>
     )
 }
